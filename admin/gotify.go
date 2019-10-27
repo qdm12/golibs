aa0c32057533658981763aa03d31b1d62131d2c2
Quentin McGaw (desktop) <quentin.mcgaw@gmail.com>
2019-10-27 19:51:34 +0000

    added initGotify to admin

--- admin/gotify.go
+++ admin/gotify.go
@@ -4,12 +4,15 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
+	"time"
 
 	"github.com/gotify/go-api-client/v2/auth"
 	"github.com/gotify/go-api-client/v2/client"
 	"github.com/gotify/go-api-client/v2/client/message"
 	"github.com/gotify/go-api-client/v2/gotify"
 	"github.com/gotify/go-api-client/v2/models"
+	"github.com/qdm12/golibs/logging"
+	"github.com/qdm12/golibs/params"
 	"go.uber.org/zap"
 )
 
@@ -17,6 +20,28 @@ import (
 type Gotify struct {
 	client *client.GotifyREST
 	token  string
+}
+
+// InitGotify creates a Gotify client from environment variables
+// and logs warnings if the parameters are not valid.
+func InitGotify(httpClient *http.Client) *Gotify {
+	if httpClient == nil {
+		logging.Warn("Setting HTTP client to default")
+		httpClient = &http.Client{Timeout: 3 * time.Second}
+	}
+	gotifyURL, err := params.GetGotifyURL()
+	if err != nil {
+		logging.Error("parameter error", err)
+	}
+	gotifyToken, err := params.GetGotifyToken()
+	if err != nil {
+		logging.Warn(err.Error())
+	}
+	gotify, err := NewGotify(gotifyURL, gotifyToken, httpClient)
+	if err != nil {
+		logging.Warn(err.Error())
+	}
+	return gotify
 }
 
 // NewGotify creates an API client with the token for the Gotify server
