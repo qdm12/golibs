73c58685230a0477565e779a888713440b86d121
Quentin McGaw (desktop) <quentin.mcgaw@gmail.com>
2019-11-03 16:57:18 +0000

    Params changes - Duration getters have to precise a unit of time - All environment variables words are split with '_'

--- params/params.go
+++ params/params.go
@@ -37,33 +37,37 @@ func GetEnvInt(key string, defaultValue int) (n int, err error) {
 }
 
 // GetDuration gets the duration from the environment variable corresponding to the key provided.
-// If none is set, it returns defaultValue.
-func GetDuration(key string, defaultValue time.Duration) (duration time.Duration, err error) {
-	value, err := GetEnvInt(key, -1)
+// If none is set, it returns defaultValue * timeUnit.
+func GetDuration(key string, defaultValue int, timeUnit time.Duration) (duration time.Duration, err error) {
+	s := GetEnv(key, "")
+	if s == "" {
+		return time.Duration(defaultValue) * timeUnit, nil
+	}
+	value, err := strconv.Atoi(s)
 	if err != nil {
 		return duration, fmt.Errorf("duration %q: %w", key, err)
 	} else if value == 0 {
 		return duration, fmt.Errorf("duration %q cannot be 0", key)
 	} else if value < 0 {
-		return defaultValue, nil
+		return duration, fmt.Errorf("duration %q cannot be lower than 0", key)
 	}
-	return time.Duration(value) * time.Millisecond, nil
+	return time.Duration(value) * timeUnit, nil
 }
 
 // GetHTTPTimeout returns the HTTP client timeout duration in milliseconds
-// from the environment variable HTTPTIMEOUT
-func GetHTTPTimeout(defaultValue time.Duration) (duration time.Duration, err error) {
-	duration, err = GetDuration("HTTPTIMEOUT", defaultValue)
+// from the environment variable HTTP_TIMEOUT
+func GetHTTPTimeout(defaultMilliseconds int) (duration time.Duration, err error) {
+	duration, err = GetDuration("HTTP_TIMEOUT", defaultMilliseconds, time.Millisecond)
 	if err != nil {
 		return duration, err
 	}
-	return duration * time.Millisecond, nil
+	return duration, nil
 }
 
 // GetListeningPort obtains and checks the listening port
-// from the environment variable LISTENINGPORT
+// from the environment variable LISTENING_PORT
 func GetListeningPort() (listeningPort string, err error) {
-	listeningPort = GetEnv("LISTENINGPORT", "8000")
+	listeningPort = GetEnv("LISTENING_PORT", "8000")
 	uid := os.Geteuid()
 	warning, err := verifyListeningPort(listeningPort, uid)
 	zap.L().Warn(warning)
@@ -71,9 +75,9 @@ func GetListeningPort() (listeningPort string, err error) {
 }
 
 // GetRootURL obtains and checks the root URL
-// from the environment variable ROOTURL
+// from the environment variable ROOT_URL
 func GetRootURL() (rootURL string, err error) {
-	rootURL = GetEnv("ROOTURL", "/")
+	rootURL = GetEnv("ROOT_URL", "/")
 	if err := verifyRootURL(rootURL); err != nil {
 		return rootURL, err
 	}
@@ -83,36 +87,36 @@ func GetRootURL() (rootURL string, err error) {
 }
 
 // GetDatabaseDetails obtains the SQL database details from the
-// environment variables SQLUSER, SQLPASSWORD and SQLDBNAME
+// environment variables SQL_USER, SQL_PASSWORD and SQL_DBNAME
 func GetDatabaseDetails() (hostname, user, password, dbName string, err error) {
-	hostname = GetEnv("sqlhost", "postgres")
+	hostname = GetEnv("SQL_HOST", "postgres")
 	if err := verifyHostname(hostname); err != nil {
 		return hostname, user, password, dbName,
 			fmt.Errorf("Postgres parameters: %w", err)
 	}
 	// TODO port
 	return hostname,
-		GetEnv("SQLUSER", "postgres"),
-		GetEnv("SQLPASSWORD", "postgres"),
-		GetEnv("SQLDBNAME", "postgres"),
+		GetEnv("SQL_USER", "postgres"),
+		GetEnv("SQL_PASSWORD", "postgres"),
+		GetEnv("SQL_DBNAME", "postgres"),
 		nil
 }
 
 // GetRedisDetails obtains the Redis details from the
-// environment variables REDISHOST, REDISPORT and REDISPASSWORD
+// environment variables REDIS_HOST, REDIS_PORT and REDIS_PASSWORD
 func GetRedisDetails() (hostname, port, password string, err error) {
-	hostname = GetEnv("REDISHOST", "redis")
+	hostname = GetEnv("REDIS_HOST", "redis")
 	if err := verifyHostname(hostname); err != nil {
 		return hostname, port, password,
 			fmt.Errorf("Redis parameters: %w", err)
 	}
-	port = GetEnv("redisport", "6379")
+	port = GetEnv("REDIS_PORT", "6379")
 	if err := verification.VerifyPort(port); err != nil {
 		return hostname, port, password,
 			fmt.Errorf("Redis parameters: %w", err)
 	}
 	return hostname, port,
-		GetEnv("redispassword", ""),
+		GetEnv("REDIS_PASSWORD", ""),
 		nil
 }
 
@@ -135,9 +139,9 @@ func GetPath(key, defaultValue string) (path string, err error) {
 }
 
 // GetLoggerEncoding obtains the logging encoding for Zap
-// from the environment variable LOGENCODING
+// from the environment variable LOG_ENCODING
 func GetLoggerEncoding() (encoding string, err error) {
-	s := GetEnv("LOGENCODING", "json")
+	s := GetEnv("LOG_ENCODING", "json")
 	s = strings.ToLower(s)
 	if s != "json" && s != "console" {
 		return "", fmt.Errorf("logger encoding %q unrecognized", s)
@@ -146,9 +150,9 @@ func GetLoggerEncoding() (encoding string, err error) {
 }
 
 // GetLoggerLevel obtains the logging level for Zap
-// from the environment variable LOGLEVEL
+// from the environment variable LOG_LEVEL
 func GetLoggerLevel() (level zapcore.Level, err error) {
-	s := GetEnv("LOGLEVEL", "info")
+	s := GetEnv("LOG_LEVEL", "info")
 	switch strings.ToLower(s) {
 	case "info":
 		return zap.InfoLevel, nil
@@ -164,9 +168,9 @@ func GetLoggerLevel() (level zapcore.Level, err error) {
 }
 
 // GetNodeID obtains the node instance ID from the environment variable
-// NODEID
+// NODE_ID
 func GetNodeID() (nodeID int, err error) {
-	s := GetEnv("NODEID", "0")
+	s := GetEnv("NODE_ID", "0")
 	nodeID, err = strconv.Atoi(s)
 	if err != nil {
 		return 0, fmt.Errorf("Node ID %q is not a valid integer", nodeID)
@@ -186,16 +190,16 @@ func GetURL(key, defaultValue string) (*url.URL, error) {
 }
 
 // GetGotifyURL obtains the URL for Gotify server
-// from the environment variable GOTIFYURL.
+// from the environment variable GOTIFY_URL.
 // It returns a nil URL if no value is found.
 func GetGotifyURL() (*url.URL, error) {
-	return GetURL("GOTIFYURL", "")
+	return GetURL("GOTIFY_URL", "")
 }
 
 // GetGotifyToken obtains the token for the app on the Gotify server
-// from the environment variable GOTIFYTOKEN.
+// from the environment variable GOTIFY_TOKEN.
 func GetGotifyToken() (token string, err error) {
-	token = GetEnv("GOTIFYTOKEN", "")
+	token = GetEnv("GOTIFY_TOKEN", "")
 	if token == "" {
 		return "", fmt.Errorf("Gotify token not provided")
 	}
