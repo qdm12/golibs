fe3b2ce434bfcead1a7a695074a14efdc7e141dd
Quentin McGaw (desktop) <quentin.mcgaw@gmail.com>
2020-01-05 17:37:43 +0000

    Refactor gotify.go and mock added

--- admin/gotify.go
+++ admin/gotify.go
@@ -4,71 +4,40 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
-	"time"
 
 	"github.com/gotify/go-api-client/v2/auth"
 	"github.com/gotify/go-api-client/v2/client"
 	"github.com/gotify/go-api-client/v2/client/message"
 	"github.com/gotify/go-api-client/v2/gotify"
 	"github.com/gotify/go-api-client/v2/models"
-	"github.com/qdm12/golibs/logging"
-	"github.com/qdm12/golibs/params"
 )
 
-// Gotify contains the Gotify API client and the token for the application
-type Gotify struct {
+// Gotify is a Gotify client
+type Gotify interface {
+	Ping() error
+	Notify(title string, priority int, content string, args ...interface{}) error
+}
+
+type gotifyImpl struct {
 	client *client.GotifyREST
 	token  string
 }
 
-// InitGotify creates a Gotify client from environment variables
-// and logs warnings if the parameters are not valid.
-func InitGotify(httpClient *http.Client, p params.EnvParams) *Gotify {
-	if httpClient == nil {
-		logging.Warn("Setting HTTP client to default")
-		httpClient = &http.Client{Timeout: 3 * time.Second}
-	}
-	gotifyURL, err := p.GetGotifyURL()
-	if err != nil {
-		logging.Err(err)
-	}
-	gotifyToken, err := p.GetGotifyToken()
-	if err != nil {
-		logging.Werr(err)
-	}
-	gotify, err := NewGotify(gotifyURL, gotifyToken, httpClient)
-	if err != nil {
-		logging.Werr(err)
-	}
-	return gotify
+// NewGotify creates an API client with the token for the Gotify server
+func NewGotify(URL url.URL, token string, httpClient *http.Client) Gotify {
+	client := gotify.NewClient(&URL, httpClient)
+	return &gotifyImpl{client: client, token: token}
 }
 
-// NewGotify creates an API client with the token for the Gotify server
-func NewGotify(URL *url.URL, token string, httpClient *http.Client) (g *Gotify, err error) {
-	if URL == nil {
-		return &Gotify{}, fmt.Errorf("Gotify URL not provided")
-	} else if token == "" {
-		return &Gotify{}, fmt.Errorf("Gotify token not provided")
+func (g *gotifyImpl) Ping() error {
+	if _, err := g.client.Version.GetVersion(nil); err != nil {
+		return fmt.Errorf("cannot communicate with Gotify server: %w", err)
 	}
-	client := gotify.NewClient(URL, httpClient)
-	_, err = client.Version.GetVersion(nil)
-	if err != nil {
-		return &Gotify{}, fmt.Errorf("cannot communicate with Gotify server: %w", err)
-	}
-	return &Gotify{client: client, token: token}, nil
+	return nil
 }
 
 // Notify sends a notification to the Gotify server
-func (g *Gotify) Notify(title string, priority int, content string, args ...interface{}) {
-	if err := g.notify(title, priority, content, args...); err != nil {
-		logging.Errorf("Gotify error: %s", err)
-	}
-}
-
-func (g *Gotify) notify(title string, priority int, content string, args ...interface{}) error {
-	if g.client == nil {
-		return nil
-	}
+func (g *gotifyImpl) Notify(title string, priority int, content string, args ...interface{}) error {
 	content = fmt.Sprintf(content, args...)
 	params := message.NewCreateMessageParams()
 	params.Body = &models.MessageExternal{
