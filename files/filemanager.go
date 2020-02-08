00356bdc9e96229864da1727a4bb0d05c009b3b1
Quentin McGaw (desktop) <quentin.mcgaw@gmail.com>
2020-02-08 09:23:15 -0500

    Added chmod to filemanager

--- files/filemanager.go
+++ files/filemanager.go
@@ -12,6 +12,7 @@ type FileManager interface {
 	DirectoryExists(filePath string) (exists bool, err error)
 	GetOwnership(filePath string) (userID, groupID int, err error)
 	GetUserPermissions(filePath string) (read, write, execute bool, err error)
+	SetUserPermissions(filepath string, mod os.FileMode) error
 	ReadFile(filePath string) (data []byte, err error)
 	WriteLinesToFile(filePath string, lines []string, options ...WriteOptionSetter) error
 	Touch(filePath string, options ...WriteOptionSetter) error
@@ -26,6 +27,7 @@ type fileManager struct {
 	mkdirAll    func(path string, perm os.FileMode) error
 	writeFile   func(filename string, data []byte, perm os.FileMode) error
 	chown       func(name string, uid int, gid int) error
+	chmod       func(name string, mod os.FileMode) error
 }
 
 func NewFileManager() FileManager {
@@ -37,5 +39,6 @@ func NewFileManager() FileManager {
 		mkdirAll:    os.MkdirAll,
 		writeFile:   ioutil.WriteFile,
 		chown:       os.Chown,
+		chmod:       os.Chmod,
 	}
 }
--- files/permissions.go
+++ files/permissions.go
@@ -2,6 +2,7 @@ package files
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/phayes/permbits"
 )
@@ -14,4 +15,14 @@ func (f *fileManager) GetUserPermissions(filePath string) (read, write, execute 
 	}
 	permissions := permbits.FileMode(info.Mode())
 	return permissions.UserRead(), permissions.UserWrite(), permissions.UserExecute(), nil
+}
+
+func (f *fileManager) SetUserPermissions(filepath string, mod os.FileMode) error {
+	exists, err := f.FileExists(filepath)
+	if err != nil {
+		return fmt.Errorf("cannot set user permissions: %w", err)
+	} else if !exists {
+		return fmt.Errorf("file %q does not exist", filepath)
+	}
+	return f.chmod(filepath, mod)
 }
