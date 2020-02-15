97f4a70f16bdd8731cdc80d15afb9af73220c8cd
Quentin McGaw (desktop) <quentin.mcgaw@gmail.com>
2020-02-15 14:10:21 -0500

    Add missing random init when creating HTTP client

--- network/requests.go
+++ network/requests.go
@@ -42,6 +42,7 @@ func NewClient(timeout time.Duration) Client {
 			"Mozilla/5.0 (iPad; CPU OS 11_0 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) Version/11.0 Mobile/15A5341f Safari/604.1",
 			"Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1",
 		},
+		random: random.NewRandom(),
 	}
 }
 
