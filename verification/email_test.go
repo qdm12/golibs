cdd0db6721ee099e17e2ac0e0ffe7d340f105af5
Quentin McGaw <quentin.mcgaw@gmail.com>
2024-10-02 15:26:35 +0000

    chore(verification): fix Test_ValidateEmail

--- verification/email_test.go
+++ verification/email_test.go
@@ -6,34 +6,34 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 func Test_ValidateEmail(t *testing.T) {
 	t.Parallel()
 	tests := map[string]struct {
-		email    string
-		mxLookup func(name string) ([]*net.MX, error)
-		err      error
+		email      string
+		mxLookup   func(name string) ([]*net.MX, error)
+		errWrapped error
+		errMessage string
 	}{
 		"Invalid email format": {
-			"aa",
-			nil,
-			ErrEmailFormatNotValid,
+			email:      "aa",
+			errWrapped: ErrEmailFormatNotValid,
+			errMessage: "email format is not valid: aa",
 		},
 		"Valid email format but not existing": {
-			"aa@aa.aa",
-			func(_ string) ([]*net.MX, error) {
+			email: "aa@aa.aa",
+			mxLookup: func(_ string) ([]*net.MX, error) {
 				return nil, errors.New("not existing")
 			},
-			errors.New("email host is not reachable: for host aa.aa: not existing"),
+			errWrapped: ErrEmailHostUnreachable,
+			errMessage: "email host is not reachable: for host aa.aa: not existing",
 		},
 		"Valid email format and existing": {
-			"aa@aa.aa",
-			func(_ string) ([]*net.MX, error) {
+			email: "aa@aa.aa",
+			mxLookup: func(_ string) ([]*net.MX, error) {
 				return nil, nil
 			},
-			nil,
 		},
 	}
 	for name, tc := range tests {
@@ -42,11 +42,9 @@ func Test_ValidateEmail(t *testing.T) {
 			t.Parallel()
 			v := &Verifier{mxLookup: tc.mxLookup, Regex: NewRegex()}
 			err := v.ValidateEmail(tc.email)
-			if tc.err != nil {
-				require.Error(t, err)
-				assert.Equal(t, tc.err.Error(), err.Error())
-			} else {
-				assert.NoError(t, err)
+			assert.ErrorIs(t, err, tc.errWrapped)
+			if tc.errWrapped != nil {
+				assert.EqualError(t, err, tc.errMessage)
 			}
 		})
 	}
